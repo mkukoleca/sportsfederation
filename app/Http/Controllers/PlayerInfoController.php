33bd6fc0f24f03c66b2686eb51fc7ff4191e377e
Duffman88 <nikoladbn@hotmail.com>
2019-10-07 20:08:45 +0200

    Model mcr , migration

---
+++ app/Http/Controllers/PlayerInfoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PlayerInfo;
+use Illuminate\Http\Request;
+
+class PlayerInfoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PlayerInfo  $playerInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PlayerInfo $playerInfo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PlayerInfo  $playerInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PlayerInfo $playerInfo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PlayerInfo  $playerInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PlayerInfo $playerInfo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PlayerInfo  $playerInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PlayerInfo $playerInfo)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_10_07_174105_create_player_infos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayerInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('player_infos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('description');
+            $table->string('position');
+            $table->float('height');
+            $table->float('weight');
+            $table->integer('jerseyNumber');
+            $table->date('dateOfBirth');
+            $table->string('citizenship');
+            $table->string('clubHistory');
+            $table->string('currentClub');
+            $table->string('selection');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('player_infos');
+    }
+}
