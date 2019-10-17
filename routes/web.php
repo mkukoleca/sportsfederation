c919506d6d945cde1248b9a37c453bb983fa32fa
jelenamihajlovic24 <jmihajlovic84@gmail.com>
2019-10-17 22:49:44 +0200

    Resolved conflict on web.php

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//Club
-Route::get('/clubs','IndexController@index' );
-Route::get('/newClub', 'ClubController@store' );
-Route::post('/newClub', 'ClubController@store')->name('create');
-Route::get('/clubs','ClubController@index');
 
 //Club
 Route::get('/','ClubController@index');
