ac706616c7da2e57a8562f1eaf9b0280e9b2e6ce
Duffman88 <nikoladbn@hotmail.com>
2019-10-22 13:28:55 +0200

    Responsive navbar and CSS

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,18 +1,30 @@
 
-<header id="header">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<header id="header"> 
+  
+  <script>function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }</script>
 
-<nav id="nav-menu-container">
-        <ul class="nav-menu">
-          <li class="menu-active"><a href="/">Početna stranica</a></li>
-          <li><a href="/clubs">Clubs</a></li>
-          <li><a href="/staff">Staff</a></li>
-          <li><a href="/selection">Selection</a></li>
-          <li><a href="/event">Event</a></li>
-          <li><a href="/games">Games</a></li>
-          <li><a href="/players">Player info</a></li>
-        <ul>
-            
-</nav>
+  <div class="topnav" id="myTopnav">
+    <a href="/" class="active">Home</a>
+    <a href="/clubs">Clubs</a>
+    <a href="/staff">Staff</a>
+    <a href="/event">Event</a>
+    <a href="/games">Games</a>
+    <a href="/players">Players</a>
+    <a href="/selection">Selection</a>
+    <a class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+  </div> 
+
+ 
       
   </header>
 <!--
@@ -29,3 +41,5 @@
 <button></button>
 
  -->
+
+ 
