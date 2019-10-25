5ba6a1e386bfc7e5ea2c33732e7d248be1d45df3
Duffman88 <nikoladbn@hotmail.com>
2019-10-25 16:20:51 +0200

    profile img

--- resources/views/federation.blade.php
+++ resources/views/federation.blade.php
@@ -463,7 +463,7 @@ button:hover, a:hover {
                                 </div>
                             </div>
                             <div class="card" style="width:200px">
-                                <img class="card-img-top" src="img/team-1.jpg">
+                                <img class="card-img-top" src="img/rafaelo.jpg">
                                 <div class="card-body">
                                     <h4 class="card-title">Nikola Dafinić</h4>
                                     <p class="card-text">Član tima Kornjače.</p>
